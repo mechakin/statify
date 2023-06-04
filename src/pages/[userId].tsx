f6d6d679e77ba00a891f93719315c5b40e973a9b
mechakin <barrientosryan83@gmail.com>
2023-06-04 07:07:10 -0400

    add redirect, edit schema, add basic user page

---
+++ src/pages/[userId].tsx
@@ -0,0 +1,13 @@
+import { PageLayout } from "~/components/layout";
+
+const UserPage = () => {
+  return (
+    <>
+      <PageLayout>
+        <h1>hi</h1>
+      </PageLayout>
+    </>
+  );
+};
+
+export default UserPage;
